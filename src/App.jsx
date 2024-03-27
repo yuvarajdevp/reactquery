edcaaa8ee9fadbbebd5f26cc4b543a3d9816eee9
yuvaraj <yuvarajk1615@gmail.com>
2024-03-27 10:14:53 +0530

    create single api fetch and errorhandling , settimeout for wait

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { useQuery } from '@tanstack/react-query';
+import Loading from './components/loading.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    isLoading,
+    data: postQuery,
+    isError,
+  } = useQuery({
+    queryKey: ["posts,"],
+    queryFn: async () => {
+      await wait(1000); // Introduce a delay of 3000 milliseconds (3 seconds)
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const postQuery = await response.json();
+      console.log(postQuery);
+      return {data:postQuery}; 
+    }
+  });
+
+  if (isError) {
+    return <span>There is no Data Here</span>;
+  }
+
+  console.log(postQuery?.data,"postQuery")
+
 
   return (
     <>
       <div>
-        <h1>React Query</h1>
-        <h1>git</h1>
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          postQuery?.data?.map(post => (
+            <ul key={post.id}>
+              <li>{post.title}</li>
+              <li>{post.body}</li>
+            </ul>
+          ))
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+function wait(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration);
+  });
+}
+
+export default App;
---
+++ src/components/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div>
+      <div class="spinner-border text-danger" role="status">
+  <span class="sr-only"></span>
+</div>
+    </div>
+  )
+}
